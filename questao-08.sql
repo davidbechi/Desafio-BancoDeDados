8f1f726b7eaecec220529a60d339ddd0d683e4d1
davidbechi <52297085+davidbechi@users.noreply.github.com>
2022-02-16 19:35:57 -0300

    new resolution

--- questao-08.sql
+++ questao-08.sql
@@ -1,4 +1,5 @@
 
 8- Mostre quantos pedidos foram feitos por mês no ano de 2022 (caso você tenha registros neste ano, senão escolha um ano que você tenha cadastrado - Novamente pense em COUNT e GROUP BY).
 
-	select data_pedido, count(numero) as pedidos_feitos from pedido group by data_pedido;
+	select month(data_pedido), count(numero) from pedido where year (data_pedido) = 2022
+ group by month (data_pedido) order by month (data_pedido);
