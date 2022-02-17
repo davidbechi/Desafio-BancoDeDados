4f73675a7f6f7a28a4d20d7b3d8a2944d7349d5e
davidbechi <52297085+davidbechi@users.noreply.github.com>
2022-02-16 21:56:56 -0300

    update single quotes

--- questao-06.sql
+++ questao-06.sql
@@ -1,5 +1,5 @@
 
 -- 6- Quantos produtos há em cada departamento? Exiba o nome do departamento e a quantidade de produtos que há em cada um. (pense em SUM e GROUP BY)
 
-	select departamento_codigo, departamento.nome, count(produto.codigo) as produtos_em_cada_dep from produto inner join departamento on produto.departamento_codigo = 		departamento.codigo
+	select departamento_codigo, departamento.nome, count(produto.codigo) as 'produtos_em_cada_dep' from produto inner join departamento on produto.departamento_codigo = 		departamento.codigo
 		group by departamento_codigo;
